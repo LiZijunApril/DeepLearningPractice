55865a4e56fea0c5a9786d8c9ad51d2e15f282f9
LiZijunApril <313736805@qq.com>
2024-03-10 14:25:59 +0800

    RNN高级API实现

--- rnn.py
+++ rnn.py
@@ -1,80 +1,61 @@
 # %%
-import math
 import torch
 from torch import nn 
-from torch.nn import functional as F
-from utils import dataset, gpu
-# %%
-batch_size, num_steps = 32, 35
+from torch.nn import functional as F 
+from utils import dataset, predictor, trainnet
+
+batch_size = 32
+num_steps = 35
 train_iter, vocab = dataset.load_data_time_machine(batch_size, num_steps)
 
-F.one_hot(torch.tensor([0, 2]), len(vocab))
+# 高级api实现rnn
+num_hiddens = 256
+rnn_layer = nn.RNN(len(vocab), num_hiddens)
 
+# 使用张量来初始化状态，它的形状是(1, batch_size, num_hiddens)
+state = torch.zeros((1, batch_size, num_hiddens))
+state.shape
 # %%
-X = torch.arange(10).reshape((2, 5))
-# print(F.one_hot(X.T, 28))
-# F.one_hot(X.T, 28).shape
-
-# 初始化模型参数
-def get_params(vovab_size, num_hiddens, device):
-    num_inputs = num_outputs = vovab_size
-    
-    def normal(shape):
-        return torch.randn(size=shape, device=device) * 0.01
-    
-    # Hidden layer parameters
-    W_xh = normal((num_inputs, num_hiddens))
-    W_hh = normal((num_hiddens, num_hiddens))
-    b_h = torch.zeros(num_hiddens, device=device)
-    
-    # Output layer paramters
-    W_hq = normal((num_hiddens, num_outputs))
-    b_q = torch.zeros(num_outputs, device=device)
-    
-    # Attach gradients (附加梯度)
-    params = [W_xh, W_hh, b_h, W_hq, b_q]
-    for param in params:
-        param.requires_grad_(True)
-
-    return params
-
-# 为了定义一个RNN，我们首先需要一个 init_rnn_state函数来在初始化时返回隐藏状态。
-def init_rnn_state(batch_size, num_hiddens, device):
-    return (torch.zeros((batch_size, num_hiddens), device=device), )
-
-# 下面的rnn函数定义了如何在一个时间步内计算隐状态和输出
-def rnn(inputs, state, params):
-    # inputs的形状为（时间步数，批量大小，词表大小）
-    W_xh, W_hh, b_h, W_hq, b_q = params
-    H, = state
-    outputs = []
-    # Shape of 'X': ('batch_size', 'vocab_size')
-    for X in inputs:
-        H = torch.tanh(torch.mm(X, W_xh) + torch.mm(H, W_hh) + b_h)
-        Y = torch.mm(H, W_hq) + b_q
-        outputs.append(Y)
-    
-    return torch.cat(outputs, dim=0), (H,)
-
-# 创建一个类打包这些函数
-class RNNModelScratch(object):
-    def __init__(self, vocab_size,  num_hiddens, device, get_params, init_state, forward_fn):
-        self.vocab_size, self.num_hiddens = vocab_size, num_hiddens
-        self.params = get_params(vocab_size, num_hiddens, device)
-        self.init_state, self.forward_fn = init_state, forward_fn
+X = torch.rand(size=(num_steps, batch_size, len(vocab)))
+Y, state_new = rnn_layer(X, state)
+Y.shape, state_new.shape
+# %% 为完整的循环神经网络定义一个RNNModel类。rnn_layer只包含隐藏的循环层。我们还需要创建一个单独的输出层
+class RNNModel(nn.Module):
+    """The RNN model"""
+    def __init__(self, rnn_layer, vocab_size, **kwargs):
+        super(RNNModel, self).__init__(**kwargs)
+        self.rnn = rnn_layer
+        self.vocab_size = vocab_size
+        self.num_hiddens = self.rnn.hidden_size
+        # 如果RNN是双向的，num_direction应该是2，否则是1
+        if not self.rnn.bidirectional:
+            self.num_direactions = 1
+            self.linear = nn.Linear(self.num_hiddens, self.vocab_size)
+        else:
+            self.num_direactions = 2
+            self.linear = nn.Linear(self.num_hiddens * 2, self.vocab_size)
+            
+    def forward(self, inputs, state):
+        X = F.one_hot(inputs.T.long(), self.vocab_size)
+        X = X.to(torch.float32)
+        Y, state = self.rnn(X, state)
+        # 全连接层首先将Y的形状改成（时间步数*批量大小，隐藏单元数）
+        # 它的输出形状为（时间步数*批量大小， 词表大小）
+        output = self.linear(Y.reshape((-1, Y.shape[-1])))
+        return output, state
+    def begin_state(self, device, batch_size=1):
+        if not isinstance(self.rnn, nn.LSTM):
+            # nn.GRU 以张量作为隐藏层
+            return torch.zeros((self.num_direactions * self.rnn.num_layers, batch_size, self.num_hiddens), device=device)
+        else:
+            # nn.LSTM 以元组作为隐藏层
+            return (torch.zeros((self.num_hiddens * self.rnn.num_layers, batch_size, self.num_hiddens), device=device), torch.zeros((self.num_direactions * self.rnn.num_layers, batch_size, self.num_hiddens), device=device))
         
-    def __call__(self, X, state):
-        X = F.one_hot(X.T, self.vocab_size).type(torch.float32)
-        return self.forward_fn(X, state, self.params)
-    
-    def begin_state(self, batch_size, device):
-        return self.init_state(batch_size, self.num_hiddens, device)
-    
-num_hiddens = 512
-net = RNNModelScratch(len(vocab), num_hiddens, 'mps', get_params, init_rnn_state, rnn)
-state = net.begin_state(X.shape[0], 'mps')
-Y, new_state = net(X.to('mps'), state)
-Y.shape, len(new_state), new_state[0].shape
-    
-
+DEVICE = 'mps'
+net = RNNModel(rnn_layer, vocab_size=len(vocab))
+net = net.to(DEVICE)
+predictor.predict_ch8('time traveller', 10, net, vocab, DEVICE)
 # %%
+num_epochs, lr = 500, 1
+trainnet.train_ch8(net, train_iter, vocab, lr, num_epochs, DEVICE)
+# %%000

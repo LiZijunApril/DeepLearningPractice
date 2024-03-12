6299e1446be29c8745c3b55fa9cd30de80e08c98
LiZijunApril <313736805@qq.com>
2024-03-12 15:33:23 +0800

    新建了DRNN深度循环神经网络

---
+++ D-RNN.py
@@ -0,0 +1,16 @@
+import torch
+from torch import nn 
+from utils import dataset, netStructure, trainnet
+
+batch_size, num_steps = 32, 35
+train_iter, vocab = dataset.load_data_time_machine(batch_size, num_steps)
+
+vocab_size, num_hiddens, num_layers = len(vocab), 256, 2
+num_inputs = vocab_size
+device = 'cpu'
+lstm_layer = nn.LSTM(num_inputs, num_hiddens, num_layers)
+model = netStructure.RNNModel(lstm_layer, len(vocab))
+model = model.to(device)
+
+num_epochs, lr = 500, 2
+trainnet.train_ch8(model, train_iter, vocab, lr, num_epochs, device)

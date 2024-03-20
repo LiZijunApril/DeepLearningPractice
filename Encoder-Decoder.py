91815f78679821f01eb51ae2ca6cac2463e0d739
LiZijunApril <313736805@qq.com>
2024-03-20 16:05:37 +0800

    预测序列的评估

--- Encoder-Decoder.py
+++ Encoder-Decoder.py
@@ -1,5 +1,6 @@
 # %%
 import torch
+import math
 from torch import nn
 from utils import plot, timer,  trainnet, dataset
 
@@ -148,7 +149,11 @@ def train_seq2seq(net, data_iter, lr, num_epochs, tgt_vocab, device):
                 if "weight" in param:
                     nn.init.xavier_uniform_(m._parameters[param])
     net.apply(xavier_init_weights)
-    net.to(device)
+    try:
+        net.to(device)
+    except:
+        net.to(device)
+
     optimizer = torch.optim.Adam(net.parameters(), lr=lr)
     loss =  MaskedSoftmaxCELoss()
     net.train()
@@ -186,4 +191,62 @@ decoder = Seq2SeqDecoder(
     len(tgt_vocab), embed_size, num_hiddens, num_layers, dropout)
 net = EncoderDecoder(encoder, decoder)
 train_seq2seq(net, train_iter, lr, num_epochs, tgt_vocab, device)
+
+# %% 预测
+def predict_seq2seq(net, src_sentence, src_vocab, tgt_vocab, num_steps, device, save_attention_weights=False):
+    """Predict for sequence to sequence"""
+    #* Set 'net' to eval mode for inference
+    net.eval()
+    src_tokens = src_vocab[src_sentence.lower().split(' ')] + [src_vocab['<eos>']]
+    enc_valid_len = torch.tensor([len(src_tokens)], device=device)
+    src_tokens = dataset.truncate_pad(src_tokens, num_steps, src_vocab['<pad>'])
+    #* Add the batch size
+    enc_X = torch.unsqueeze(torch.tensor(src_tokens, dtype=torch.long, device=device), dim=0)
+    enc_outputs = net.encoder(enc_X, enc_valid_len)
+    dec_state = net.decoder.init_state(enc_outputs, enc_valid_len)
+    #* Add the batch axis
+    dec_X = torch.unsqueeze(torch.tensor([tgt_vocab['<bos>']], dtype=torch.long, device=device), dim=0)
+    output_seq, attention_weight_seq = [], []
+    
+    for _ in range(num_steps):
+        Y, dec_state = net.decoder(dec_X, dec_state)
+        # 我们使用预测可能性最大的词元，作为解码器在下一个时间步的输入
+        # dec_X = Y.argmax(dim=2)
+        dec_X = Y.argmax(dim=2)
+
+        pred = dec_X.sequeeze(dim=0).type(torch.init32).item()
+        # 保存注意力权重
+        if save_attention_weights:
+            attention_weight_seq.append(net.decoder.attention_weights)
+        # 一旦<eos>被预测出，输出序列的预测就结束了
+        if pred == tgt_vocab['<eos>']:
+            break
+        output_seq.append(pred)
+    return ' '.join(tgt_vocab.to_tokrnd(output_seq)), attention_weight_seq
+
+import collections
+# %% BLEU
+def bleu(pred_seq: str, label_seq: str, k: str):
+    """Compute the BLEU"""
+    pred_tokens, label_tokens = pred_seq.split(' '), label_seq.split(' ')
+    len_pred, len_label = len(pred_tokens), len(label_tokens)
+    score = math.exp(min(0, 1 - len_label / len_pred))
+    for n in range(1, k+1):
+        num_matches, label_subs = 0, collections.defaultdict(int)
+        for i in range(len_label - n + 1):
+            label_subs[' '.join(label_tokens[i: i + n])] += 1          
+        for i in range(len_pred - n + 1):
+            if label_subs[' '.join(pred_tokens[i: i + n])] > 0:
+                num_matches += 1
+                label_subs[' '.join(pred_tokens[i: i + n])] -= 1
+        score *= math.pow(num_matches / (len_pred - n + 1), math.pow(0.5, n))
+    return score
+
+engs = ['go .', "i lost .", 'he\'s calm .', 'i\'m home .']
+fras = ['va !', 'j\'ai perdu .', 'il est calme .', 'je suis chez moi .']
+for eng, fra in zip(engs, fras):
+    translation, attention_weight_seq = predict_seq2seq(
+        net, eng, src_vocab, tgt_vocab, num_steps, device
+    )
+    print(f'{eng} => {translation}, bleu {bleu(translation, fra, k=2):.3f}')
 # %%
